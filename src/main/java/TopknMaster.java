840a2b2708541bd41fe69980c3d810c109f30342
万少 <344277934@qq.com>
2017-07-11 11:20:01 +0800

    add code

---
+++ src/main/java/TopknMaster.java
@@ -0,0 +1,161 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TopknMaster负责接收题目给定的k,n值，并且将信息发送给TopknWorker1和TopknWorker2 Created by wanshao on 2017/6/29.
+ */
+public class TopknMaster implements Runnable {
+
+    public TopknMaster(int port){
+        this.port = port;
+    }
+
+    // port address of TopknWorker
+    private int port;
+
+    // 比赛输入
+    private static long k;
+    private static int n;
+
+    private static Logger logger = LoggerFactory.getLogger(TopknMaster.class);
+
+    @Override
+    public void run() {
+        this.startMasterThread(port);
+    }
+
+    public static void main(String[] args) {
+        logger.info("init some args....");
+        // 获取比赛使用的k,n值
+        k = Long.valueOf(args[0]);
+        n = Integer.valueOf(args[1]);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(new TopknMaster(5527));
+        executorService.submit(new TopknMaster(5528));
+
+    }
+
+    /**
+     * @return
+     * @throws IOException
+     */
+    private void startMasterThread(int port) {
+        ServerSocketChannel serverSocket = null;
+        try {
+
+            serverSocket = ServerSocketChannel.open();
+            serverSocket.bind(new InetSocketAddress(port));
+            logger.info("Port 5527 and 5528 is open for connecting...");
+
+            // 处理一次请求来回即可
+            SocketChannel socketChannel = serverSocket.accept();
+            socketChannel.configureBlocking(true);
+            Thread writeThread = new Thread(new MasterWriteThread(socketChannel));
+            Thread readThread = new Thread(new MasterReadThread(socketChannel));
+            writeThread.start();
+            readThread.start();
+            readThread.join();
+
+            // 处理完毕后关闭socket
+            socketChannel.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    class MasterReadThread implements Runnable {
+
+        private final Logger logger = LoggerFactory.getLogger(MasterReadThread.class);
+        private final int READ_BUFFER_SIZE = 1024;
+        private SocketChannel socketChannel;
+
+        public MasterReadThread(SocketChannel socketChannel){
+            this.socketChannel = socketChannel;
+        }
+
+        @Override
+        public void run() {
+            getResultAndProcess();
+        }
+
+        /**
+         * 获取worker的结果并且处理
+         */
+        private void getResultAndProcess() {
+
+            try {
+                logger.info("Begin to read result from worker " + socketChannel.getRemoteAddress());
+                ByteBuffer readBuffer = ByteBuffer.allocate(READ_BUFFER_SIZE);
+                while (socketChannel.read(readBuffer) != -1) {
+                    readBuffer.flip();
+
+                    // do something with the result
+                    processResult(readBuffer);
+                    readBuffer.clear();
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 对结果数据做一些处理
+         *
+         * @param readBuffer
+         */
+        private void processResult(ByteBuffer readBuffer) {
+            System.out.println(new String(readBuffer.array()));
+        }
+    }
+
+    /**
+     * master可以使用该线程发送比赛数据给worker,并且读取处理结果
+     */
+    public class MasterWriteThread implements Runnable {
+
+        private static final int WRITE_BUFFER_SIZE = 1024;
+        private final Logger logger = LoggerFactory.getLogger(MasterWriteThread.class);
+        private SocketChannel socketChannel;
+
+        public MasterWriteThread(SocketChannel socketChannel){
+            this.socketChannel = socketChannel;
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                logger.info("Begin to send input to worker " + socketChannel.getRemoteAddress());
+                ByteBuffer sendBuffer = ByteBuffer.allocate(WRITE_BUFFER_SIZE);
+                sendBuffer.clear();
+                // 发送比赛输入
+                sendBuffer.putLong(k);
+                sendBuffer.putInt(n);
+                sendBuffer.flip();
+                while (sendBuffer.hasRemaining()) {
+                    socketChannel.write(sendBuffer);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/TopknWorker.java
@@ -0,0 +1,138 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Worker1会接收master提供的题目信息，并且得到计算结果后返回给master Created by wanshao on 2017/6/29.
+ */
+public class TopknWorker {
+
+    private static final int masterPort = 5527;
+    private static String masterHostAddress;
+    private final String dataDirPath = "/Users/wanshao/work/final_data";
+    private Logger logger = LoggerFactory.getLogger(TopknWorker.class);
+
+    private long k;
+    private int n;
+
+    public static void main(String[] args) throws IOException {
+
+        masterHostAddress = args[0];
+        new TopknWorker().connect(masterHostAddress, masterPort);
+    }
+
+    public void connect(String host, int port) {
+        logger.info("begin to connect " + host + ":" + port);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open(new InetSocketAddress(masterHostAddress, masterPort));
+            socketChannel.configureBlocking(true);
+            logger.info("Connected to server: " + socketChannel);
+            Thread readThread = new Thread(new WorkerReadThread(socketChannel));
+            readThread.start();
+            readThread.join();
+            Thread writeThread = new Thread(new WorkerWriteThread(socketChannel, k, n));
+            writeThread.start();
+            writeThread.join();
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    class WorkerWriteThread implements Runnable {
+
+        private static final int WRITE_BUFFER_SIZE = 1024;
+        private SocketChannel socketChannel;
+        private Logger logger = LoggerFactory.getLogger(WorkerReadThread.class);
+        // 比赛输入
+        private long k;
+        private int n;
+
+        public WorkerWriteThread(SocketChannel socketChannel, long k, int n){
+            this.socketChannel = socketChannel;
+            this.k = k;
+            this.n = n;
+        }
+
+        @Override
+        public void run() {
+            processAndSendResult(k, n);
+        }
+
+        /**
+         * 根据比赛输入来计算结果,并且发送结果给master
+         *
+         * @return
+         */
+        private void processAndSendResult(long k, int n) {
+
+            logger.info("begin to process topKN problem");
+
+            try {
+                logger.info("Begin to send topkn result to master " + socketChannel.getRemoteAddress());
+                ByteBuffer sendBuffer = ByteBuffer.allocate(WRITE_BUFFER_SIZE);
+                // process topKN problem
+                String data = "I am worker, and I have received data from master: k is " + k + " and n is " + n;
+                byte[] sendData = data.getBytes();
+                sendBuffer.clear();
+                sendBuffer.put(sendData);
+                sendBuffer.flip();
+                while (sendBuffer.hasRemaining()) {
+                    socketChannel.write(sendBuffer);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * worker可以使用该线程发送处理后的结果给worker Created by wanshao on 2017/7/8 0008.
+     */
+    class WorkerReadThread implements Runnable {
+
+        private static final int READ_BUFFER_SIZE = 1024 * 1024;
+        private SocketChannel socketChannel;
+        private Logger logger = LoggerFactory.getLogger(WorkerReadThread.class);
+
+        public WorkerReadThread(SocketChannel socketChannel){
+            this.socketChannel = socketChannel;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                logger.info("Begin to read input data from master " + socketChannel.getRemoteAddress());
+                ByteBuffer readBuffer = ByteBuffer.allocate(READ_BUFFER_SIZE);
+                readBuffer.clear();
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBuffer.remaining() >= 0) {
+                    // do something with the result
+                    readBuffer.flip();
+                    k = readBuffer.getLong();
+                    n = readBuffer.getInt();
+                    readBuffer.clear();
+                    logger.info("Receive input data, k is " + k + " and n is " + n);
+                }
+
+                logger.info("Reading intput data from master is finished...");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
